8e3d61f17c7f5ae5ba220095dc19bec08ae6d65c
Jeromie Fernandes <jeromie@ajency.in>
2015-07-10 18:18:21 +0530

    checkout styles

--- wp-content/themes/HighendWP-child/js/custom.js
+++ wp-content/themes/HighendWP-child/js/custom.js
@@ -2,24 +2,55 @@
 window.onload = function(){
     jQuery('.woocommerce-message').hide()
 
-    jQuery('#customer_next').on('click',function(e){
+    // jQuery('#customer_next').on('click',function(e){
 
-      jQuery('#customer_details').show()
+    //   jQuery('#customer_details').show()
 
-    })
-    jQuery('#payment_next').on('click',function(e){
+    // })
 
-      jQuery('#payment_options').show()
+    jQuery('#customer_next').click(function() {
+      jQuery('.accordion-group.one').removeClass('open');
+      jQuery('.accordion-group.two').addClass('open viewed');
+    });
 
-    })
-    jQuery('#customer_back').on('click',function(e){
-
-      jQuery('#customer_details').hide()
-      jQuery('#payment_options').hide()
-      jQuery('form').clearForm()
+    jQuery('.one.viewed > .acc-title').click(function() {
+      jQuery('.accordion-group').removeClass('open');
+      jQuery('.accordion-group.one').addClass('open');
+    });
 
 
-    })
+    // jQuery('#payment_next').on('click',function(e){
+
+    //   jQuery('#payment_options').show()
+
+    // })
+    
+    jQuery('#payment_next').click(function() {
+      jQuery('.accordion-group.two').removeClass('open');
+      jQuery('.accordion-group.three').addClass('open viewed');
+    });
+
+    jQuery('.two.viewed > .acc-title').click(function() {
+      jQuery('.accordion-group').removeClass('open');
+      jQuery('.accordion-group.two').addClass('open');
+    });
+
+
+    // jQuery('#customer_back').on('click',function(e){
+
+    //   jQuery('#customer_details').hide()
+    //   jQuery('#payment_options').hide()
+    //   jQuery('form').clearForm()
+
+
+    // })
+
+    jQuery('#customer_back').click(function() {
+      jQuery('.accordion-group').removeClass('open');
+      jQuery('.accordion-group.one').addClass('open');
+    });
+
+
     jQuery('.single_add_to_cart_button').on('click' , function(e){
       e.preventDefault()
       jQuery('#variation_id').val(jQuery(e.currentTarget).val());
