49fa0342f24282428ed92c8746a827bca374b373
Jeromie Fernandes <jeromie@ajency.in>
2015-07-14 11:21:47 +0530

    styles

--- wp-content/themes/skyifirst/woocommerce/single-product/add-to-cart/variable.php
+++ wp-content/themes/skyifirst/woocommerce/single-product/add-to-cart/variable.php
@@ -24,7 +24,7 @@ global $product, $post, $woocommerce;
 			<input type="hidden" id="product_id" name="product_id" value="" />
 				<input type="hidden" id="add-to-cart" name="add-to-cart" value="" />
 					<input type="hidden" id="attribute_pa_unit_type" name="attribute_pa_unit_type" value="" />
-
+			<div class="row">
 		<?php
 		foreach ($variations as $key => $value) {
 		?>
@@ -41,24 +41,29 @@ global $product, $post, $woocommerce;
 
 
 
-			<div class="hb-woo-product-details">
-				<button type="submit" id="buy_button<?php echo $value['variation_id']?>" data-product="<?php echo $product->id; ?>" value="<?php echo $value['variation_id']?>" class="hb-woo-main-link ">
-					<h2 class="hb-woo-title"><?php echo implode('/', $value['attributes']);?></h2>
-					<div><?php echo $value['price_html'];?> all inclusive*</div>
-				</button>
-			</div>
+			
+				<div class="hb-woo-product-details col-4">
+					<button type="submit" id="buy_button<?php echo $value['variation_id']?>" data-product="<?php echo $product->id; ?>" value="<?php echo $value['variation_id']?>" class="hb-woo-main-link ">
+						<h2 class="hb-woo-title"><?php echo implode('/', $value['attributes']);?></h2>
+						<div><?php echo $value['price_html'];?> all inclusive*</div>
+					</button>
+				</div>
+			
 
 				<script>
 
+				
+				jQuery(document).ready(function(){
 				jQuery('.hb-woo-main-link').on('click' , function(e){
-					console.log("entered");
-				 e.preventDefault()
-				 jQuery('#variation_id').val(jQuery(e.currentTarget).val());
-				 jQuery('#product_id').val(jQuery(e.currentTarget).attr('data-product'));
-				   jQuery('#add-to-cart').val(jQuery(e.currentTarget).attr('data-product'));
+				console.log("entered");
+				e.preventDefault()
+				jQuery('#variation_id').val(jQuery(e.currentTarget).val());
+				jQuery('#product_id').val(jQuery(e.currentTarget).attr('data-product'));
+				jQuery('#add-to-cart').val(jQuery(e.currentTarget).attr('data-product'));
 
-				   jQuery('#attribute_pa_unit_type').val(jQuery('#attributepa_unit_type'+jQuery(e.currentTarget).val()).val());
-				 jQuery('form#myForm').submit();
+				jQuery('#attribute_pa_unit_type').val(jQuery('#attributepa_unit_type'+jQuery(e.currentTarget).val()).val());
+				jQuery('form#myForm').submit();
+				})
 				})
 
 
@@ -67,6 +72,7 @@ global $product, $post, $woocommerce;
 
 		<?php
 		}
-		?>	</form>
+		?>	</div>
+		</form>
 
 <?php
