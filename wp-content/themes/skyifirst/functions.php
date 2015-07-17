a02da12a9bb9f81572cd63927c405a97f6c424e5
Surekha Rane <surekha@ajency.in>
2015-07-17 18:03:08 +0530

    disabled select2

--- wp-content/themes/skyifirst/functions.php
+++ wp-content/themes/skyifirst/functions.php
@@ -760,3 +760,16 @@ foreach ($customer_orders as $customer_order) {
 }
 
 add_action('wp_ajax_check_cheque_no','check_cheque_no');
+
+add_action( 'wp_enqueue_scripts', 'mgt_dequeue_stylesandscripts', 100 );
+
+function mgt_dequeue_stylesandscripts() {
+    if ( class_exists( 'woocommerce' ) ) {
+        wp_dequeue_style( 'select2' );
+        wp_deregister_style( 'select2' );
+
+        wp_dequeue_script( 'select2');
+        wp_deregister_script('select2');
+
+    }
+}
